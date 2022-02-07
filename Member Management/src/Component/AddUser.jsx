2260f113db8bfc6feed2cd1ba1cebc25d8bb9c8e
AnIkeT126 <mishraaniket522@gmail.com>
2022-02-07 19:46:45 +0530

    first commit

---
+++ Member Management/src/Component/AddUser.jsx
@@ -0,0 +1,70 @@
+import react, { useState } from 'react';
+import { FormGroup, FormControl, InputLabel, Input, Button, makeStyles, Typography } from '@material-ui/core';
+import { addUser } from '../Service/api';
+import { useHistory } from 'react-router-dom';
+
+const initialValue = {
+    name: '',
+    discord_handle: '',
+    email: '',
+    phone: '',
+    domain: ''
+}
+
+const useStyles = makeStyles({
+    container: {
+        width: '50%',
+        margin: '5% 0 0 25%',
+        '& > *': {
+            marginTop: 20
+        }
+    }
+})
+
+const AddUser = () => {
+    const [user, setUser] = useState(initialValue);
+    const { name, discord_handle, email, phone,domain } = user;
+    const classes = useStyles();
+    let history = useHistory();
+
+    const onValueChange = (e) => {
+        console.log(e.target.value);
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    const addUserDetails = async() => {
+        await addUser(user);
+        history.push('./all');
+    }
+
+    return (
+        <FormGroup className={classes.container}>
+            <Typography variant="h4">Add User</Typography>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Name</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name='name' value={name} id="my-input" />
+            </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Discord Handle</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name='discord_handle' value={discord_handle} id="my-input" />
+            </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Email</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name='email' value={email} id="my-input"/>
+            </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Phone</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name='phone' value={phone} id="my-input" />
+            </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Domain</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name='domain' value={domain} id="my-input" />
+            </FormControl>
+            <FormControl>
+                <Button variant="contained" color="primary" onClick={() => addUserDetails()}>Add User</Button>
+            </FormControl>
+        </FormGroup>
+    )
+}
+
+export default AddUser;
